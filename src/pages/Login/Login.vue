a2bd396afde1755ae83a06b20767ea9aed87c24f
JuliusZhuu <44996257+JuliusZhuu@users.noreply.github.com>
2021-01-14 21:23:40 +0800

    登录功能界面绘制以及前台校验

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -5,44 +5,50 @@
       <div class="login_header">
         <h2 class="login_logo">硅谷外卖</h2>
         <div class="login_header_title">
-          <a href="javascript:;" class="on">短信登录</a>
-          <a href="javascript:;">密码登录</a>
+          <a href="javascript:;" :class="{on:isSelect}" @click="isSelect=true">短信登录</a>
+          <a href="javascript:;" :class="{on:!isSelect}" @click="isSelect=false">密码登录</a>
         </div>
       </div>
       <div class="login_content">
         <form>
-          <div class="on">
+          <!--验证码登录-->
+          <div :class="{on:isSelect}">
             <section class="login_message">
-              <input type="tel" maxlength="11" placeholder="手机号">
-              <button disabled="disabled" class="get_verification">获取验证码</button>
+              <input type="tel" maxlength="11" placeholder="手机号" v-model="phone">
+              <button :disabled="!rightButton" class="get_verification"
+                      :class="{right_button:rightButton}" @click.prevent="getCode">
+                {{computeTime>0?`已发送${computeTime}s`:'获取验证码'}}
+              </button>
             </section>
             <section class="login_verification">
-              <input type="tel" maxlength="8" placeholder="验证码">
+              <input type="tel" maxlength="8" placeholder="验证码" v-model="code">
             </section>
             <section class="login_hint">
               温馨提示：未注册硅谷外卖帐号的手机号，登录时将自动注册，且代表已同意
               <a href="javascript:;">《用户服务协议》</a>
             </section>
           </div>
-          <div>
+          <!--密码登录-->
+          <div :class="{on:!isSelect}">
             <section>
               <section class="login_message">
-                <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名">
+                <input type="text" maxlength="11" placeholder="手机/邮箱/用户名" v-model="name">
               </section>
               <section class="login_verification">
-                <input type="tel" maxlength="8" placeholder="密码">
-                <div class="switch_button off">
-                  <div class="switch_circle"></div>
-                  <span class="switch_text">...</span>
+                <input type="text" maxlength="8" placeholder="密码" v-model="pwd" v-if="showPwd">
+                <input type="password" maxlength="8" placeholder="密码" v-model="pwd" v-else>
+                <div class="switch_button " :class="showPwd===true?'on':'off'" @click="showPwd=!showPwd">
+                  <div class="switch_circle" :class="{right:showPwd}"></div>
+                  <span class="switch_text">{{showPwd===true?'abc':'...'}}</span>
                 </div>
               </section>
               <section class="login_message">
-                <input type="text" maxlength="11" placeholder="验证码">
+                <input type="text" maxlength="11" placeholder="验证码" v-model="captcha">
                 <img class="get_verification" src="./images/captcha.svg" alt="captcha">
               </section>
             </section>
           </div>
-          <button class="login_submit">登录</button>
+          <button class="login_submit" @click.prevent="login">登录</button>
         </form>
         <a href="javascript:;" class="about_us">关于我们</a>
       </div>
@@ -54,11 +60,76 @@
 </template>
 
 <script>
+  import Dialog from 'vant/lib/Dialog'
+  import 'vant/lib/dialog/style'
+
   export default {
     name: 'Login',
+    data () {
+      return {
+        isSelect: true,//默认是短信登录,false为密码登录
+        code: null,//短信验证码
+        phone: null,//手机号
+        computeTime: 0,//倒计时
+        showPwd: false,//是否显示密码,默认不显示
+        name: null,//登录用户名
+        pwd: null,//密码
+        captcha: null,//图片验证码
+      }
+    },
+    computed: {
+      rightButton () {
+        //以1开头,后面必须是数字且是10位
+        return /^1\d{10}$/.test(this.phone)
+      }
+    },
     methods: {
       back (path) {
         this.$router.replace(path)
+      },
+      /**
+       * 获取验证码
+       */
+      getCode () {
+        //只有当前值没有开始计时
+        if (this.computeTime === 0) {
+          //开启计时器
+          this.computeTime = 30
+          const intervalId = setInterval(() => {
+            this.computeTime--
+            if (this.computeTime === 0) {
+              clearInterval(intervalId)
+            }
+          }, 1000)
+        }
+      },
+      /**
+       * 登录验证
+       */
+      login () {
+        //判断是手机短信验证码还是账号密码
+        //短信登录
+        if (this.isSelect) {
+          //取出手机号和短信验证码
+          const {rightButton, phone, code} = this
+          //校验手机号
+          if (!rightButton) {
+            Dialog({message: '手机号输入不正确'})
+            //校验验证码,必须是6位数字
+          } else if (!/^\d{6}$/.test(code)) {
+            Dialog({message: '验证码必须是6位数字'})
+          }
+          //账号密码登录
+        } else {
+          const {name, pwd, captcha} = this
+          if (name === null) {
+            Dialog({message: '用户名必须指定'})
+          } else if (pwd === null) {
+            Dialog({message: '密码必须指定'})
+          } else if (captcha === null) {
+            Dialog({message: '验证码必须输入'})
+          }
+        }
       }
     }
   }
@@ -138,6 +209,9 @@
                 font-size 14px
                 background transparent
 
+                &.right_button
+                  color black
+
             .login_verification
               position relative
               margin-top 16px
@@ -182,6 +256,9 @@
                   background #fff
                   box-shadow 0 2px 4px 0 rgba(0, 0, 0, .1)
                   transition transform .3s
+
+                  &.right //主要这这个地方设置移动
+                    transform translateX(30px)
 
             .login_hint
               margin-top 12px
